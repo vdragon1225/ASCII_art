778ab3f6c3fdde22aeccef1f2c0d3004589cd316
vdragon1225 <148491922+vdragon1225@users.noreply.github.com>
2024-10-17 22:57:24 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import PIL.Image
+
+# ascii characters used to build the output text
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+# resize image according to a new width
+def resize(image, new_width=100):
+    width, height = image.size
+    ratio = height / width / 1.65
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+#convert each pixel to grayscale
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return grayscale_image
+
+#convert pixels to a string of ASCII characters
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = ""
+    for pixel_value in pixels:
+        ascii_str += ASCII_CHARS[pixel_value // 25]
+    return ascii_str
+
+def main(new_width=100):
+    # attempt to open image from user-input
+    path = input("Enter a valid pathname to an iamge:\n")
+    try:
+        image = PIL.Image.open(path)
+    except:
+        print(path, "is not a valid pathname to an image.")
+
+    new_image_data = pixels_to_ascii(grayify(resize(image)))
+
+    # format
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, pixel_count, new_width)])
+
+    # print result
+    print(ascii_image)
+
+    # save result to "ascii_image.txt"
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
