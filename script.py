0a617d30b811df3347ba9aebc26cede2b3264c9d
vdragon1225 <148491922+vdragon1225@users.noreply.github.com>
2024-11-13 14:08:46 +0000

    second commit

--- script.py
+++ script.py
@@ -4,25 +4,24 @@ import PIL.Image
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 
 # resize image according to a new width
-def resize(image, new_width=100):
+def resize_image(image, new_width=100):
     width, height = image.size
     ratio = height / width / 1.65
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
-    return resized_image
+    return(resized_image)
 
-#convert each pixel to grayscale
+# convert each pixel to grayscale
 def grayify(image):
     grayscale_image = image.convert("L")
-    return grayscale_image
+    return(grayscale_image)
 
-#convert pixels to a string of ASCII characters
+# convert pixels to a string of ASCII characters
 def pixels_to_ascii(image):
     pixels = image.getdata()
-    ascii_str = ""
-    for pixel_value in pixels:
-        ascii_str += ASCII_CHARS[pixel_value // 25]
-    return ascii_str
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)
+
 
 def main(new_width=100):
     # attempt to open image from user-input
@@ -32,16 +31,17 @@ def main(new_width=100):
     except:
         print(path, "is not a valid pathname to an image.")
 
-    new_image_data = pixels_to_ascii(grayify(resize(image)))
+    # convert image to ASCII
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
 
     # format
     pixel_count = len(new_image_data)
-    ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, pixel_count, new_width)])
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
 
     # print result
     print(ascii_image)
 
-    # save result to "ascii_image.txt"
+    #save result to "ascii_image.txt"
     with open("ascii_image.txt", "w") as f:
         f.write(ascii_image)
 
