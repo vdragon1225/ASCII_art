c2401c9d510d02563e53facdfaa6583f0d42784f
vdragon1225 <148491922+vdragon1225@users.noreply.github.com>
2024-11-16 00:41:30 +0000

    third commit

--- script.py
+++ script.py
@@ -1,43 +1,42 @@
 import PIL.Image
-
 # ascii characters used to build the output text
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+#ASCII_CHARS = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
 # resize image according to a new width
-def resize(image, new_width=100):
+def resize_image(image, new_width=100):
     width, height = image.size
     ratio = height / width / 1.65
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
-    return resized_image
+    return(resized_image)
 
-#convert each pixel to grayscale
+# convert each pixel to grayscale
 def grayify(image):
     grayscale_image = image.convert("L")
-    return grayscale_image
+    return(grayscale_image)
 
-#convert pixels to a string of ASCII characters
+# convert pixels to a string of ascii characters
 def pixels_to_ascii(image):
     pixels = image.getdata()
-    ascii_str = ""
-    for pixel_value in pixels:
-        ascii_str += ASCII_CHARS[pixel_value // 25]
-    return ascii_str
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)
 
 def main(new_width=100):
     # attempt to open image from user-input
     path = input("Enter a valid pathname to an image:\n")
     try:
         image = PIL.Image.open(path)
-    except:
-        print(path, "is not a valid pathname to an image.")
+    except Exception as e:
+        print(path, "is not valid pathname to an image.")
 
-    new_image_data = pixels_to_ascii(grayify(resize(image)))
+    # convert image to ASCII
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
 
     # format
     pixel_count = len(new_image_data)
-    ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, pixel_count, new_width)])
-
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+ 
     # print result
     print(ascii_image)
 
